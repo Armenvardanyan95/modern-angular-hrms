a0084d1f04113d60fd1f4446af1864e53faeb1fb
Steven Edwards <steven@stephenwithav.com>
2023-11-05 10:07:26 -0500

    ng new hrms --defaults --standalone --routing

--- src/app/infrastructure/types/employee-form.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export type EmployeeForm = {
-  firstName: FormControl<string>;
-  lastName: FormControl<string>;
-  email: FormControl<string>;
-  position: FormControl<string>;
-  level: FormControl<string>;
-};
--- src/app/infrastructure/types/project.ts
+++
@@ -1,8 +0,0 @@
-export type Project = {
-    id: number;
-    name: string;
-    description: string;
-    image: string;
-    employees: number[];
-    subProjectIds: number[];
-}
